93d731187f60a701f7bd2e0139ef6dfd146482e3
blackwellsmith <blackwellsmithiv@gmail.com>
2019-12-16 13:46:25 -0500

    added index and show to ChoresController

--- app/controllers/chores_controller.rb
+++ app/controllers/chores_controller.rb
@@ -1,2 +1,13 @@
 class ChoresController < ApplicationController
+    before_action :authenticate_user!
+
+    def index
+        chores = current_user.chores 
+        render json: chores.to_json
+    end
+
+    def show
+        chore = Chore.find(params[:id])
+        authorize_owner_resource(dog)
+    end
 end
