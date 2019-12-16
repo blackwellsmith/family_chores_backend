2db7134f71e5cf7172bfa107b0c88f70eb5483d7
blackwellsmith <blackwellsmithiv@gmail.com>
2019-12-16 09:11:12 -0500

    changed blacklist table to match docs

--- db/migrate/20191216135926_create_jwt_blacklist.rb
+++ db/migrate/20191216135926_create_jwt_blacklist.rb
@@ -1,9 +1,10 @@
 class CreateJwtBlacklist < ActiveRecord::Migration[5.2]
   def change
     create_table :jwt_blacklist do |t|
-      t.string :jti
+      t.string :jti, null: false
 
-      t.timestamps
+      t.datetime :exp, null: false
     end
+    add_index :jwt_blacklist, :jti
   end
 end
