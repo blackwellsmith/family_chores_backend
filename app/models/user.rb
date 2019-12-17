9bdc527ef3504db05bca64bdedd60b413882e380
blackwellsmith <blackwellsmithiv@gmail.com>
2019-12-17 09:27:01 -0500

    added dependent destroy in user model on chores

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   #devise :database_authenticatable, :registerable,
     #     :recoverable, :rememberable, :validatable
-    has_many :chores
+    has_many :chores, dependent: :destroy
 
   devise :database_authenticatable,
          :jwt_authenticatable,
--- jsfrontend/src/pageManagers/signup.js
+++ jsfrontend/src/pageManagers/signup.js
@@ -5,7 +5,12 @@ class SignupPage{
 
     get staticHTML() {
         return (`
+        <h2>Sign Up</h2>
         <form>
+        <div class="form-group">
+          <label for="name">Name</label>
+          <input type="name" class="form-control" id="name" placeholder="Name">
+        </div>
         <div class="form-group">
           <label for="email">Email address</label>
           <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email">
