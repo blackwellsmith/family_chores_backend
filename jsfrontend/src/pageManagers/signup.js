bee6c581b6dda81703af3b8861c441d5703be461
blackwellsmith <blackwellsmithiv@gmail.com>
2019-12-17 09:52:29 -0500

    added required on form input tag

--- jsfrontend/src/pageManagers/signup.js
+++ jsfrontend/src/pageManagers/signup.js
@@ -9,16 +9,16 @@ class SignupPage{
         <form>
         <div class="form-group">
           <label for="name">Name</label>
-          <input type="name" class="form-control" id="name" placeholder="Name">
+          <input type="name" class="form-control" id="name" placeholder="Name" required>
         </div>
         <div class="form-group">
           <label for="email">Email address</label>
-          <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email">
+          <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email" required>
           <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
         </div>
         <div class="form-group">
           <label for="password">Password</label>
-          <input type="password" class="form-control" id="password" placeholder="Password">
+          <input type="password" class="form-control" id="password" placeholder="Password" required>
         </div>
         <button type="submit" class="btn btn-primary">Submit</button>
         </form>
