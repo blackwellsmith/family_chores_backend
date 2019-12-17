18d8bc024fa608744ae0f2c50996a064ed752f7d
blackwellsmith <blackwellsmithiv@gmail.com>
2019-12-17 08:54:20 -0500

    added container class to page-container div added signup html to pageManagers/signup in a staticmethod

--- jsfrontend/src/pageManagers/signup.js
+++ jsfrontend/src/pageManagers/signup.js
@@ -3,7 +3,24 @@ class SignupPage{
         this.container = container
     }
 
+    get staticHTML() {
+        return (`
+        <form>
+        <div class="form-group">
+          <label for="email">Email address</label>
+          <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email">
+          <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+        </div>
+        <div class="form-group">
+          <label for="password">Password</label>
+          <input type="password" class="form-control" id="password" placeholder="Password">
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+        `)
+    }
+
     render() {
-        this.container.innerHTML = '<h1>yo</h1>'
+        this.container.innerHTML = this.staticHTML
     }
 }
