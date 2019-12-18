96f73e0fa12f6409344a17e6614da7ab928be4c7
blackwellsmith <blackwellsmithiv@gmail.com>
2019-12-17 19:36:11 -0500

    built staticHTML on welcome page and added script tags for the new pages

--- jsfrontend/src/pageManagers/welcome.js
+++ jsfrontend/src/pageManagers/welcome.js
@@ -2,7 +2,7 @@ class WelcomePage extends PageManager{
     get staticHTML() {
         return (`
         <h1>Welcome to The Legendary Chores App!</h1>
-        
-       `)
+        <h3>Please<a id="signup">Signup</a> or <a id="login">Login</a></h3>
+        `)
    }
 }
