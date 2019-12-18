c56991bc3911ebef2a2168b6eb8e43b8ec9a5851
blackwellsmith <blackwellsmithiv@gmail.com>
2019-12-17 20:26:12 -0500

    built out login page

--- jsfrontend/src/pageManagers/welcome.js
+++ jsfrontend/src/pageManagers/welcome.js
@@ -4,14 +4,24 @@ class WelcomePage extends PageManager{
         this.signupLink = this.container.querySelector('a#signup')
         this.loginLink = this.container.querySelector('a#login')
 
-        this.signupLink.addEventListener('click', handleSignupClick.bind(this))
-        this.loginLink.addEventListener('click', handleLoginClick.bind(this))
+        this.signupLink.addEventListener('click', this.handleSignupClick.bind(this))
+        this.loginLink.addEventListener('click', this.handleLoginClick.bind(this))
 
+    }
+
+    handleSignupClick(e) {
+        e.preventDefault() 
+        this.redirect('signup')
+    }
+
+    handleLoginClick(e) {
+        e.preventDefault()
+        this.redirect('login')
     }
 
     get staticHTML() {
         return (`
-        <h1>Welcome to The Legendary Chores App!</h1>
+        <h1>Welcome to The Legendary Chores Anti-Procrastanator App!</h1>
         <h3>Please <a href="#" id="signup">Signup</a> or <a href="#" id="login">Login</a></h3>
         `)
    }
