3059f75fc209778f02d833e7a31483315013d6e1
blackwellsmith <blackwellsmithiv@gmail.com>
2019-12-19 12:02:56 -0500

    created chore adapter and connected new chore submit to async in chore add script tag for chore adapter

--- jsfrontend/src/components/user.js
+++ jsfrontend/src/components/user.js
@@ -12,10 +12,25 @@ class User{
         <div>
         <br>
         <h1>Welcome ${this.name}!</h1>
+        <br>
         <h2>Looks like you have ${this.chores.length} chores left</h2>
         <ul>
           ${this.chores.map(c => c.liAndLinkHTML).join()}
         </ul>
+        <br>
+        <h2>Time for another?</h2>
+        <form id="chore">
+        <div class="form-group">
+          <label for="name">Name</label>
+          <input type="name" class="form-control" id="name" placeholder="name" required>
+        </div>
+        <div class="form-group">
+          <label for="notes">Notes</label>
+          <input type="notes" class="form-control" id="name" placeholder="name" required>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+        <br>
         </div>
         `)
     }
--- jsfrontend/src/pageManagers/profile.js
+++ jsfrontend/src/pageManagers/profile.js
@@ -3,16 +3,33 @@ class ProfilePage extends PageManager{
     constructor(container, adapter) {
         super(container)
         this.adapter = new ProfileAdapter(adapter)
+        this.choreAdapter = new ChoreAdapter(adapter)
         this.user = null
     }
 
     initBindingsAndEventListeners() {
-        return null
-    }
-
-    finalBindingsAndEventListeners() {
-        const choreList = this.container.querySelector('ul')
-        //choreList.addEventListener('dblclick', this.deleteChoreclick.bind(this))
+        this.form = this.container.querySelector('#chore')
+        
+        this.form.addEventListener('submit', this.handleSubmit.bind(this))
+      }
+    
+    async handleSubmit(e) {
+      e.preventDefault()
+      const inputs = Array.from(e.target.querySelectorAll('input'))
+      const [name, notes] = inputs.map(input => input.value)
+      
+      const params = {
+        user: {
+          name, notes
+        }
+      }
+      try {
+        await this.choreAdapter.newChore(params)
+        this.redirect('profile')
+      } catch(err){
+        //this.handleAlert(err, 'Danger')
+        this.handleError(err)
+      }
     }
     async fetchAndRenderPageResources() {
         try {
