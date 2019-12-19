ddbfc5ba2532f8e694910359c20cde0ace9e00ba
blackwellsmith <blackwellsmithiv@gmail.com>
2019-12-19 12:49:17 -0500

    created chore page for new chore added constuctor and html

--- jsfrontend/src/components/user.js
+++ jsfrontend/src/components/user.js
@@ -17,20 +17,7 @@ class User{
         <ul>
           ${this.chores.map(c => c.liAndLinkHTML).join()}
         </ul>
-        <br>
-        <h2>Time for another?</h2>
-        <form id="chore">
-        <div class="form-group">
-          <label for="name">Name</label>
-          <input type="name" class="form-control" id="name" placeholder="name" required>
-        </div>
-        <div class="form-group">
-          <label for="notes">Notes</label>
-          <input type="notes" class="form-control" id="name" placeholder="name" required>
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-        </form>
-        <br>
+        
         </div>
         `)
     }
--- jsfrontend/src/pageManagers/profile.js
+++ jsfrontend/src/pageManagers/profile.js
@@ -3,43 +3,47 @@ class ProfilePage extends PageManager{
     constructor(container, adapter) {
         super(container)
         this.adapter = new ProfileAdapter(adapter)
-        this.choreAdapter = new ChoreAdapter(adapter)
+        //this.choreAdapter = new ChoreAdapter(adapter)
         this.user = null
     }
-
+  
+  
     initBindingsAndEventListeners() {
-        this.form = this.container.querySelector('#chore')
-        
-        this.form.addEventListener('submit', this.handleSubmit.bind(this))
-      }
-    
-    async handleSubmit(e) {
-      e.preventDefault()
-      const inputs = Array.from(e.target.querySelectorAll('input'))
-      const [name, notes] = inputs.map(input => input.value)
+      //this.form = this.container.querySelector('#chore')
       
-      const params = {
-        user: {
-          name, notes
-        }
+      //this.form.addEventListener('submit', this.handleSubmit.bind(this))
+    }
+  
+  async handleSubmit(e) {
+    e.preventDefault()
+    const inputs = Array.from(e.target.querySelectorAll('input'))
+    const [name, notes] = inputs.map(input => input.value)
+    
+    const params = {
+      user: {
+        name, notes
       }
+    }
+    try {
+      await this.choreAdapter.newChore(params)
+      this.redirect('profile')
+    } catch(err){
+      //this.handleAlert(err, 'Danger')
+      this.handleError(err)
+    }
+  }
+  
+  async fetchAndRenderPageResources() {
       try {
-        await this.choreAdapter.newChore(params)
-        this.redirect('profile')
-      } catch(err){
-        //this.handleAlert(err, 'Danger')
-        this.handleError(err)
+          const userObj = await this.adapter.getUser()
+          this.user = new User(userObj)
+          this.renderOwner()
+      } catch (err) {
+          this.handleError(err)
       }
-    }
-    async fetchAndRenderPageResources() {
-        try {
-            const userObj = await this.adapter.getUser()
-            this.user = new User(userObj)
-            this.renderOwner()
-        } catch (err) {
-            this.handleError(err)
-        }
-    }
+  }
+
+    
     get staticHTML() {
         return (`
         <div class="loader"></div>
